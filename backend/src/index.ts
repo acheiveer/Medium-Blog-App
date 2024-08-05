074a98df5327e613406a740c5264966bbf470c9b
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-06 01:42:57 +0530

    migrated the schmea  and craeted signup signin route and aslo middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,88 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string,
+    JWT_SECRET: string
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+  }
+}>()
+
+//middlewares for authentications
+app.use('/message/*', async (c, next) => {
+  // here we don't use the format "Bearer token"
+  const header = c.req.header("authorization") || "";
+  const response = await verify(header,c.env.JWT_SECRET);
+
+  if(response.id){
+    await next(); 
+  }
+  else{
+    c.status(403);
+    return c.json({
+      error: "Unauthorized"
+    })
+  }
 })
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
+
+
+
+
+//signup route
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password
+    }
+  })
+  const token = await sign({id : user.id},c.env.JWT_SECRET) 
+  return c.json({
+    jwt: token 
+  });
 })
+
+
+
+//signin route
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where:{
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  if(!user){
+    c.status(403);
+    return c.json({
+      error: "user not found"
+    })
+  }
+
+  const token = await sign({id: user.id},c.env.JWT_SECRET);
+  return c.json({
+    jwt: token
+  })
+
+})
+
+
+
+
 app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
