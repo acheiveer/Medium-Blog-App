a67b5d21d42f0b69e8369c2aaee6a90f09838488
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-11 03:47:07 +0530

    Completed signup and sign in page and linked with the respective apis

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,14 +1,32 @@
 import { signupInput } from "@prabhakr4u/medium-common";
 import { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<signupInput>({
         name: "",
         email: "",
         password: ""
     })
+
+   async  function sendRequest(){
+       try {
+        const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type==="signup"? "signup": "signin"}`,
+            postInputs
+            // here in the signin route the backend will ignore the name
+        );
+        const jwt = response.data; 
+        localStorage.setItem("Token",jwt);
+        navigate("/blogs")
+       } catch (error) {
+          // request failed
+          alert("Error while signing up");
+       }
+    }
     return <div>
         <div className="h-screen flex justify-center flex-col">
             <div className="flex justify-center">
@@ -24,12 +42,12 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                         </div>
                     </div>
                     <div className="pt-8">
-                        <LabelledInput label="Name" placeholder="Prabhakar Singh.." onChange={(e) => {
+                        {type==="signup"? <LabelledInput label="Name" placeholder="Prabhakar Singh.." onChange={(e) => {
                             setPostInputs({
                                 ...postInputs,
                                 name: e.target.value
                             })
-                        }} />
+                        }} /> : null}
                         <LabelledInput label="Email" placeholder="xyz@email.com" onChange={(e) => {
                             setPostInputs({
                                 ...postInputs,
@@ -42,7 +60,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                                 password: e.target.value
                             })
                         }} />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                             {type === "signup" ? "Sign up": "Sign in"}</button>
 
                     </div>
