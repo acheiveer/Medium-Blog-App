4203aa4c71c68127883804ebc1f64eb672738620
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-13 00:00:23 +0530

    Added some conditions in blog/bilk route

--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -104,7 +104,18 @@ blogRoutes.get('/bulk', async (c) => {
         datasourceUrl: c.env.DATABASE_URL,
       }).$extends(withAccelerate())
 
-    const blogs = await prisma.post.findMany();
+    const blogs = await prisma.post.findMany({
+      select: {
+        content: true,
+        title: true,
+        id: true,
+        author:{
+          select:{
+            name: true
+          }
+        }
+      }
+    });
     return c.json({
         blogs
     }) 
