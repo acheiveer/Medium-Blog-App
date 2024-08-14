6c0c8da96eed8d35fd7827db3172871cf298b1e5
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-14 23:38:55 +0530

    Added some styles to AppBar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -3,16 +3,19 @@ import { Avatar } from "./BlogCard";
 
 export const Appbar = () => {
     return (
-        <div className="border-b justify-between flex px-10 py-4">
-            <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
-                medium
+        <div className="border-b border-gray-200 bg-white shadow-md flex items-center justify-between px-6 py-4">
+            <Link to={'/blogs'} className="text-2xl font-semibold text-gray-800 hover:text-gray-600">
+                Medium
             </Link>
-            <div className="flex items-center"> 
-               <Link to={'/publish'}>
-               <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">
-                    New
-                </button>
-               </Link>
+            <div className="flex items-center space-x-4">
+                <Link to={'/publish'}>
+                    <button
+                        type="button"
+                        className="text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-4 py-2 transition duration-150 ease-in-out"
+                    >
+                        New
+                    </button>
+                </Link>
                 <Avatar name="Prabhkar" size={"big"} />
             </div>
         </div>
