73ace1365e5283116596c36c94d18f0de39b462d
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-15 00:12:28 +0530

    Added signup button on page

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -8,12 +8,22 @@ export const Appbar = () => {
                 Medium
             </Link>
             <div className="flex items-center space-x-4">
+                <Link to={'/signup'}>
+                    <button
+                        type="button"
+                        className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-4 py-2 transition duration-150 ease-in-out"
+                    >
+                        Sign Up
+                    </button>
+                </Link>
+
+
                 <Link to={'/publish'}>
                     <button
                         type="button"
                         className="text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-4 py-2 transition duration-150 ease-in-out"
                     >
-                        New
+                        Write Blog
                     </button>
                 </Link>
                 <Avatar name="Prabhkar" size={"big"} />
